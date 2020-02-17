bb05ef83a628a667397eccddd008a96bc79ce267
Myra Fah <FahMyra@JohnDeere.com>
2020-02-17 11:51:41 -0600

    html and style updates, function to shuffle the correct answer into the incorrect answers, update to read-me to credit reference for shuffle algorithm

--- js/script.js
+++ js/script.js
@@ -5,25 +5,86 @@ const choicesElement = document.querySelector('.choices')
 const guessesElement = document.querySelectorAll('.guess')
 const correctScoreElement = document.querySelector('#correct')
 const incorrectScoreElement = document.querySelector('#incorrect')
-const categoryElement = document.querySelector('.category')
+//const categoryElement = document.querySelector('.category')
+const remainingElement = document.querySelector('.remaining')
 
 //global variables
-const howManyQuestions = 3
+let sessionToken = ''
+//categories with several questions includes:
+//9 - General Knowledge
+//11 - Entertainment - film
+//12 - Entertainment - music
+//17 - Science and Nature
+//22 - Geography
+//23 - History
 let categoryChoice = ''
+//max questions allowed by api is 50
+let howManyQuestions = 0
+let playedQuestions = 0
+let remainingQuestions = 0
+let guessSequence = []
 let questionQueue = []
+let correctAnswer = ''
 let correct = 0
 let incorrect = 0
 
-//set trivia category
+//set options
 categoryChoice = 'History'
-categoryElement.textContent = categoryChoice
+howManyQuestions = 300
 
-//qet questions
+//connect to api and retrieve a session token: including the token in question api calls will prevent duplicates in the response data while the token is active (6 hours max)
+// let sessionToken = getTriviaApiData('https://opentdb.com/api_token.php?command=request')
+// console.log(sessionToken)
+
+function getSessionToken() {
+    axios({
+        url: 'https://opentdb.com/api_token.php?command=request',
+        method: 'get'
+    })
+        .then(response => {
+            sessionToken = [response.data.token]
+        })
+        .catch(error => {
+            console.log(error)
+        })
+}
+getSessionToken()
+// getSessionToken()
+
+// function getTriviaApiData(inUrl) {
+//     //console.log(inUrl)
+//     axios({
+//         url: inUrl,
+//         method: 'get'
+//     })
+//         .then(response => {
+//             response = [response.data.results]
+//             console.log(response)
+//             return response
+//         })
+//         .catch(error => {
+//             console.log(error)
+//         })
+// }
+
+//retrieve a batch of questions for a new game
+// function getQuestions() {
+//     //clear question variable to accept new questions
+//     playedQuestions = 0
+//     questionQueue = []
+//     questionQueue = getTriviaApiData(`https://opentdb.com/api.php?amount=${howManyQuestions}&category=23&type=multiple`)
+//     questionQueue = [1]
+//     nextQuestion()
+// }
+
+//connect to open trivia database api and retrieve a batch of questions for a new game
 function getQuestions() {
     //clear question variable to accept new questions
+    playedQuestions = 0
     questionQueue = []
+    //retrieve questions data
     axios({
-        url: `https://opentdb.com/api.php?amount=${howManyQuestions}&category=23&type=multiple`,
+        url: `https://opentdb.com/api.php?amount=${howManyQuestions}&category=22&type=multiple&token=${sessionToken}`,
         method: 'get'
     })
         .then(response => {
@@ -35,29 +96,34 @@ function getQuestions() {
         })
 }
 
-//get a question
+const selectAnswer = function (event) {
+    console.log(`click! ${event}`)
+
+    //do a check on questionsRemaining... if it is the last question, end game after selection is made
+
+    // let guessClick = ''
+    // guessClick = event.target.id
+    // console.log(guessClick)
+}
+
+//load a question into the game and reduce the remaining question counter
 function nextQuestion() {
-    clearChoices()
-
     let question = questionQueue.shift()
+    playedQuestions = playedQuestions + 1
+    questionsRemaining = howManyQuestions - playedQuestions
+    remainingElement.textContent = `Answered ${playedQuestions}`
 
-    //assign question text
+    //assign question text and answer
     questionElement.textContent = translateSpecials(question.question)
-    console.log(questionElement.textContent)
+    correctAnswer = question.correct_answer
+    console.log(correctAnswer)
+    let responseChoices = (question.incorrect_answers)
+    responseChoices.push(correctAnswer)
 
-    //assign answer choices
-    let responseChoices = question.incorrect_answers
-    //console.log(responseChoices)
+    //shuffle choices and assign to dom (correct answer needs to be shuffled into incorrect answers)
+    writeChoices(responseChoices)
 
-    responseChoices.forEach(function (choice) {
-        let newP = document.createElement('p')
-        newP.textContent = choice
-        newP.classList.add('guess')
-        console.log(newP)
-        choicesElement.appendChild(newP)
-    })
-    //remove question from queue
-    console.log(questionQueue)
+    //setup listener for player's answer
     activateGuesses()
 }
 
@@ -69,40 +135,90 @@ function clearChoices() {
     }
 }
 
-const selectAnswer = function (event) {
-    console.log('click!')
-
-    let guessClick = ''
-    guessClick = event.target.id
-    console.log(guessClick)
+//shuffle choices
+function writeChoices(choices) {
+    //clear previously assigned choices
+    clearChoices()
+    //shuffle choices
+    shuffle(choices)
+    choices.forEach(function (choice) {
+        let newP = document.createElement('p')
+        newP.textContent = translateSpecials(choice)
+        newP.setAttribute('class', 'guess')
+        choicesElement.appendChild(newP)
+    })
 }
 
 function activateGuesses() {
-    guessesElement.forEach(g => {
-        g.addEventListener('click', selectAnswer)
+    guessesElement.forEach(guess => {
+        console.log(`listen ${guess}`)
+        guess.addEventListener('click', event => {
+            selectAnswer
+        })
     })
+}
+
+//Fisher-Yates Algorithm to randomly shuffle an array
+function shuffle(array) {
+    var i = 0
+        , j = 0
+        , temp = null
+
+    for (i = array.length - 1; i > 0; i -= 1) {
+        j = Math.floor(Math.random() * (i + 1))
+        temp = array[i]
+        array[i] = array[j]
+        array[j] = temp
+        return array
+    }
 }
 
 getQuestions()
 
-
-//I found some common special charachter string translations were needed on api response text  
+//testing revealed api data contains special characters needing string translation
 function translateSpecials(str) {
-    str = str.replace('&ldquo;', '"')
-    str = str.replace('&rdquo;', '"')
-    str = str.replace('&rdquo;', '"')
-    str = str.replace('&rsquo;', "'")
-    str = str.replace('&rsquo;', "'")
-    str = str.replace('&#039;', "'")
-    str = str.replace('&#039;', "'")
+    str = str.replace(('&ldquo;', 'gi'), '"')
+    str = str.replace(('&rdquo;', 'gi'), '"')
+    str = str.replace(('&quot;', 'gi'), '"')
+    str = str.replace('&amp;', '&')
     str = str.replace('&quot;', '"')
     str = str.replace('&quot;', '"')
+    str = str.replace('&quot;', '"')
+    str = str.replace('&quot;', '"')
+    str = str.replace('&quot;', '"')
+    str = str.replace(('&ocirc;', 'gi'), 'ô')
     str = str.replace('&ocirc;', 'ô')
+    str = str.replace(('&atilde;', 'gi'), 'ã')
     str = str.replace('&atilde;', 'ã')
+    str = str.replace(('&oacute;', 'gi'), 'ó')
     str = str.replace('&oacute;', 'ó')
+    str = str.replace(('&lt;', 'gi'), '<')
+    str = str.replace(('&gt;', 'gi'), '>')
+    str = str.replace(('&shy;', 'gi'), '-')
+    str = str.replace('&shy;', '-')
+    str = str.replace('&shy;', '-')
+    str = str.replace('&shy;', '-')
+    str = str.replace('&shy;', '-')
+    str = str.replace('&shy;', '-')
+    str = str.replace('&shy;', '-')
+    str = str.replace('&shy;', '-')
+    str = str.replace('&shy;', '-')
+    str = str.replace(('&deg;', 'gi'), '°')
+    str = str.replace('&deg;', '°')
+    str = str.replace(('&iacute;', 'gi'), 'í')
     str = str.replace('&oacute;', 'ó')
-    str = str.replace('&lt;', '<')
-    str = str.replace('&gt;', '>')
-
+    str = str.replace('&uuml;', 'ü')
+    str = str.replace('&ouml;', 'ö')
+    str = str.replace('&Nu;', 'ν')
+    str = str.replace('&pi;', 'π')
+    str = str.replace('&aacute', 'á')
+    str = str.replace('&Sigma;', 'Σ')
+    str = str.replace(('&#039;', 'gi'), '`')
+    str = str.replace('&#039;', '`')
+    str = str.replace('&#039;', '`')
+    str = str.replace('&#039;', '`')
+    str = str.replace('&#039;', '`')
+    str = str.replace('&#039;', '`')
+    str = str.replace('&#039;', '`')
     return str
 }
--- readme.md
+++ readme.md
@@ -1,13 +1,21 @@
-#Trivia Game
+#Trivia Game  
 
-##How to play:
+##How to play:  
 
+###Click start!  
 
+####Game options:  
+* question category  
+* how many questions 
 
+###Technologies used:  
+* Open Triva Database (https://opentdb.com) created by Pixeltail Games. LLC, a free to use, user contributed trivia question database.  
+ * With each browser refresh, a session token is obtained from the api to prevent the same question from presenting to the player during play. If the player consumes the limited number of questions available, the browser would need to be refreshed. Consequently, the player may be presented with repeat questions.
+* Google Fonts (https://fonts.google.com)  
+* HTML validated with HTML5 Validator (https://html5.validator.nu/)  
+* CSS validated with CSS Validator (https://jigsaw.w3.org/css-validator/)  
 
-##Technologies used:  
-Open Triva Database (https://opentdb.com) created by Pixeltail Games. LLC, a free to user, user contributed trivia question database.  
-  
-Google Fonts (https://fonts.google.com)
-
-
+###Acknowledgements and credits
+* Guidance for how to shuffle an array was referenced from Mike Bostock's blog-post about coding the Fisher-Yates Algorithm in javascript.      
+ * (https://bost.ocks.org/mike/shuffle)
+ * (https://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle#Fisher_and_Yates')
